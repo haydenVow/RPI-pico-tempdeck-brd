6832bea1c6ac6f2613fa500f095bb4a259af3f6d
haydenVow <93174809+haydenVow@users.noreply.github.com>
2022-01-04 15:00:32 +1100

    init

---
+++ tools/pqt-gcc/1.3.1-a-7855b0c/lib/gcc/arm-none-eabi/10.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/workdir/repo/gcc-gnu/configure --prefix=/workdir/arm-none-eabi.osx --build=x86_64-linux-gnu --host=x86_64-apple-darwin14 --target=arm-none-eabi --disable-shared --with-newlib --enable-threads=no --disable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-nls --disable-bootstrap --enable-languages=c,c++ --enable-lto --enable-static=yes --disable-libstdcxx-verbose --disable-decimal-float --enable-multilib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
